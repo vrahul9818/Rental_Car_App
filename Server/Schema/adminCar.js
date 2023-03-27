6eae6dd9904048c5677eafc02c1e4ba20bf365c4
SubodhJondhale <subodhjondhale1998@gmail.com>
2023-03-27 20:44:39 +0530

    userCarBooking Backend and frontend Car display

--- Server/Schema/adminCar.js
+++ Server/Schema/adminCar.js
@@ -2,21 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const adminCarSchema = new Schema({
-  carName: {type: String,required: true,},
-  carType: {type: String,required: true,},
-  model: {type: String,required: true,},
-  mileage: {type: String,required: true,},
-  perKm: {type: Number,required: true,},
-  availableFrom: {type: Date},
-  availableTo: {type: Date},
-  description: {type: String,required: true,},
-  images: {type: [String]},
-  carDetails: {type: String},
-  Admin_id:{type: String}
+  carName: { type: String, required: true },
+  carType: { type: String, required: true },
+  model: { type: String, required: true },
+  mileage: { type: String, required: true },
+  perKm: { type: Number, required: true },
+  availableFrom: { type: String },
+  availableTo: { type: String },
+  description: { type: String, required: true },
+  images: { type: [String] },
+  carDetails: { type: String },
+  Admin_id: { type: String },
 });
 
-const AdminCarModel = mongoose.model('Cars',adminCarSchema);
+const AdminCarModel = mongoose.model("Cars", adminCarSchema);
 
 module.exports = AdminCarModel;
-
-
